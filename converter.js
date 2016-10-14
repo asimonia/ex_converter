9879c7bad17dd3c822ac10ea06321402d12a9e77
asimonia <alex.simonian@gmail.com>
2016-10-13 19:39:55 -0500

    Add conversion

--- converter.js
+++ converter.js
@@ -8,12 +8,21 @@ function toFahrenheit () {
 
 // Get a reference to the button element in the DOM
 var button = document.getElementById("converter");
+var button2 = document.getElementById("clear");
+
+var qinput = document.getElementById("qinput");
 
 // This function should determine which conversion should
 // happen based on which radio button is selected.
+
 function determineConverter (clickEvent) {
   console.log("event", clickEvent);
 }
 
+function clearInput () {
+	qinput.value = "";
+}
+
 // Assign a function to be executed when the button is clicked
 button.addEventListener("click", determineConverter);
+button2.addEventListener("click", clearInput);
